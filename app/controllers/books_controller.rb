a6bbb77464df341fb7c5db075692d8a7d0a266e0
Nishimurashun <132205509+sHuN945@users.noreply.github.com>
2023-06-19 16:14:25 +0000

    [update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,33 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
+    @user = current_user
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
     end
   end
-  
+
   def index
-    flash[:notice] = "Welcome! You have signed up successfully."
     @book = Book.new
     @books = Book.all
     @user = current_user
-  end 
-  
+  end
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @user = current_user
-  end 
-  
+  end
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -37,17 +37,17 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "本当に消しますか？"
     redirect_to '/books'
-  end 
-  
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
-  end 
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update, :index]
-  
-  
+  before_action :is_matching_login_user, only: [:edit, :update ]
+
+
   def index
-    flash[:notice] = "Welcome! You have signed up successfully."
     @book = Book.new
     @books = Book.all
     @user = current_user
-  end 
-  
+    flash[:notice] = "Welcome! You have signed up successfully."
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @user = current_user
   end
 
   def edit
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to books_path
-    end 
+      redirect_to user_path(@user.id)
+    end
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -30,21 +31,20 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-  end 
-  
+  end
+
   private
-  
+
   def user_params
-    params.require(:user).permit(:user_name, :image, :introduction)
-  end 
-  
+    params.require(:user).permit(:user_name, :profile_image, :introduction)
+  end
+
   def is_matching_login_user
-    @user = User.find(params[:id])
-    unless @user.id == current_user.id
+    user = User.find(params[:id])
+    unless user.id == current_user.id
       redirect_to user_path(current_user)
     end
-  end 
-  
-  
-  
+  end
+
+
 end
